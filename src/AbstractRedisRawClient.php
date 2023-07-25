28f1d27332bc1366fd2b453bc831e960cf6cbc06
Jonathan Artaud <jonathan.artaud@allopneus.com>
2023-07-25 22:00:55 +0200

    test version two

--- src/AbstractRedisRawClient.php
+++ src/AbstractRedisRawClient.php
@@ -14,7 +14,8 @@ abstract class AbstractRedisRawClient implements RedisRawClientInterface
     public const REDIS_CLIENT_LIBRARY = 'RedisClient';
 
     public $redis;
-    protected LoggerInterface $logger;
+    /** @var LoggerInterface */
+    protected $logger;
 
     public function connect($hostname = '127.0.0.1', $port = 6379, $db = 0, $password = null): RedisRawClientInterface
     {
